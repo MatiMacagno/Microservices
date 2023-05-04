311b6a1cd1f534c6ca7a17cc61ef99e81cdbe56a
MatiMacagno <matiasmacagno10@gmail.com>
2023-05-03 22:58:13 -0300

    Connecting order-service and inventory-service. Service Discovery using Netflix Eureka.

--- order-service/src/main/java/com/store/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/store/orderservice/OrderServiceApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-OrderServiceApplication {
-
+public class OrderServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
-
 }
